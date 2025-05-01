013d49d2e537249229998b9582e5eb190d99ad55
Brian Chin <brian.chin@gmail.com>
2025-05-01 10:38:03 -0700

    Update some extra info about the site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -80,6 +80,10 @@ const config: Config = {
         },
       ],
     },
+    colorMode: {
+      respectPrefersColorScheme: true,
+      defaultMode: 'light',
+    },
     footer: {
       style: 'dark',
       links: [
@@ -115,7 +119,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Brian Chin. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
