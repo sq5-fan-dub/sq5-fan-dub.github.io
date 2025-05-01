292a20db420a12fc39c33a205c94aad907ab3845
Brian Chin <brian.chin@gmail.com>
2025-05-01 10:31:53 -0700

    Clean up the blog section until we're ready to use it

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'sq5-fan-dub/img/docusaurus-social-card.jpg',
+    image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'SQ5: Re-Mutilated',
       logo: {
@@ -72,7 +72,7 @@ const config: Config = {
           position: 'left',
           label: 'About',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        // {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/sq5-fan-dub',
           label: 'GitHub',
@@ -96,29 +96,21 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
+              href: 'https://discord.gg/2XH9JAKRk5',
             },
           ],
         },
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            // {
+            //   label: 'Blog',
+            //   to: '/blog',
+            // },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/sq5-fan-dub',
             },
           ],
         },
